ca4b3670b44b20daabdf39361f78ca2f5a25ecad
DEPSTRCZ <jiri.edelmann@gmail.com>
2022-11-10 08:00:42 +0000

    Fix

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import csv
 import requests
 import json
 def check(ip):
-    result = requests.get("https://api.abuseipdb.com/api/v2/check", headers = {"Accept": "application/json","Key": apikey2}, params = {"ipAddress": ip}).json()
+    result = requests.get("https://api.abuseipdb.com/api/v2/check", headers = {"Accept": "application/json","Key": apikey}, params = {"ipAddress": ip}).json()
     try:
         if result["data"]["totalReports"] > 1:
             print(f'{result["data"]["ipAddress"]}','=',f'[{result["data"]["countryCode"]}]','-',f'Times Reported: {result["data"]["totalReports"]}','-',f'Confidace: {result["data"]["abuseConfidenceScore"]}%','-',f'Last Report: {result["data"]["lastReportedAt"]}',f'(https://www.abuseipdb.com/check/{ip})')
@@ -12,7 +12,7 @@ def check(ip):
         pass
     try:
         if result["errors"][0]["detail"] == "The ip address must be a valid IPv4 or IPv6 address (e.g. 8.8.8.8 or 2001:4860:4860::8888).":
-            print(ip,"="," Invalid IP!")
+            print(ip,"=","Invalid IP!")
     except:
         pass
 apikey = input("Please enter an API key\n")
